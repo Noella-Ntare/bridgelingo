33da17bbd0fd9d48d3f5e919a10eec96e5724350
Lalisa <l.iriza@alustudent.com>
2026-03-29 15:31:49 +0200

    test: implement basic smoke test for MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,9 @@
 import 'package:bridgelingo/main.dart';
 
 void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
+ testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-  });
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+});
 }
